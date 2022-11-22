526f95dd45393ce33291f2c3dc0a3c3bb852324b
nicomaci <86749503+nicomaci@users.noreply.github.com>
2022-11-21 23:57:29 -0500

    PQArray Initial Commit
    PQArray class and insert function defined.
    
    Todo for Stage 1: 
    - extractMax()
    - extractValidRecipient() (extractMax() until valid recipient is found, then reinsert extracted recipients)

---
+++ PQArray.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Nicolas Macias on 11/21/22.
+//
+
+#include "PQArray.h"
+
+PQArray::PQArray() {}
+
+void PQArray::insert(int n) {
+    this->queue.push_back(n);
+    int child = this->queue.size() - 1;
+    int parent = (child - 1) / 2;
+
+    while (parent >= 0 && this->queue[parent] < this->queue[child]) {
+        int temp = this->queue[parent];
+        this->queue[parent] = this->queue[child];
+        this->queue[child] = temp;
+        child = parent;
+        parent = (child - 1) / 2;
+    }
+}
+
+int PQArray::extract() {
+    this->queue[0] =
+}
