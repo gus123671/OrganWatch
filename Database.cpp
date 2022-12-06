b49307263618e4dede887dc3c81a79a3adea21b6
Augustus Rodriguez <augustusrodriguez007@gmail.com>
2022-12-06 02:48:03 -0500

    minor changes

--- Database.cpp
+++ Database.cpp
@@ -6,60 +6,61 @@
 */
 
 #include "Database.h"
+#include <fstream>
+#include <sstream>
 
 Database::Database() 
 {
     this->donors = vector<Donor>();
 }
 
-//void Database::loadData(std::string file, std::vector<Donor>& donors)
-//{
-//    ifstream inFile(file);
-//
-//    std::string lineFromFile;
-//    getline(inFile, lineFromFile); // skip header
-//
-//    while (getline(inFile, lineFromFile)) {
-//        istringstream stream(lineFromFile);
-//
-//        string name;
-//        string ageStr;
-//        string organToDonate;
-//        string location;
-//
-//        getline(stream, name, ',');
-//        getline(stream, ageStr, ',');
-//        getline(stream, organToDonate, ',');
-//        getline(stream, location, '\n');
-//
-//        Donor d = new Donor(name, stoi(ageStr), organToDonate, location);
-//        donors.push_back(d);
-//    }
-//}
-//
-//void Database::loadRecipientData(std::string file, vector<Recipient>& recipients)
-//{
-//    {
-//        ifstream inFile(file);
-//
-//        std::string lineFromFile;
-//        getline(inFile, lineFromFile); // skip header
-//
-//        while (getline(inFile, lineFromFile)) {
-//            istringstream stream(lineFromFile);
-//
-//            string name;
-//            string ageStr;
-//            string organToRecieve;
-//            string location;
-//
-//            getline(stream, name, ',');
-//            getline(stream, ageStr, ',');
-//            getline(stream, organToDonate, ',');
-//            getline(stream, location, ',');
-//
-//            Recipient r = new Recipient(name, stoi(ageStr), organToRecieve, location, 0);
-//            recipients.push_back(r);
-//        }
-//    }
-//}
+void Database::loadDonorData(std::string file, std::vector<Donor>& donors)
+{
+   ifstream inFile(file);
+
+   std::string lineFromFile;
+   getline(inFile, lineFromFile); // skip header
+
+   while (getline(inFile, lineFromFile)) 
+   {
+       istringstream stream(lineFromFile);
+
+       string name;
+       string ageStr;
+       string organToDonate;
+       string location;
+
+       getline(stream, name, ',');
+       getline(stream, ageStr, ',');
+       getline(stream, organToDonate, ',');
+       getline(stream, location, '\n');
+
+       donors.push_back(Donor(name, stoi(ageStr), organToDonate, location));
+   }
+}
+
+void Database::loadRecipientData(std::string file, vector<Recipient>& recipients)
+{
+   {
+       ifstream inFile(file);
+
+       std::string lineFromFile;
+       getline(inFile, lineFromFile); // skip header
+
+       while (getline(inFile, lineFromFile)) {
+           istringstream stream(lineFromFile);
+
+           string name;
+           string ageStr;
+           string organToReceive;
+           string location;
+
+           getline(stream, name, ',');
+           getline(stream, ageStr, ',');
+           getline(stream, organToReceive, ',');
+           getline(stream, location, ',');
+
+           recipients.push_back(Recipient(name, stoi(ageStr), organToReceive, location, 0));
+       }
+   }
+}
